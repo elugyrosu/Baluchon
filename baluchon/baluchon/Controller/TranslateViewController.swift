ec3025938d4ed3a46f51630f33635d912c63cdc9
leeloomulti <leeloomulti@gmail.com>
2019-06-14 13:06:15 +0200

    first commit

---
+++ baluchon/baluchon/Controller/TranslateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TranslateViewController.swift
+//  baluchon
+//
+//  Created by Jordan MOREAU on 14/06/2019.
+//  Copyright © 2019 Jordan MOREAU. All rights reserved.
+//
+
+import UIKit
+
+class TranslateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
